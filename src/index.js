15f73ba56f5a69c3f0b43dfa9fbe57e756d454b5
Valeriya Ivashkevich <ivashkevich03@gmail.com>
2023-08-06 14:15:28 +0300

    feat: write the function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
 
-if (str.length === 0 || bracketsConfig === 0){
+const bracketsConfigObj = bracketsConfig.reduce((key, value) => {
+  key[value[1]] = value[0];
+  return key
+}, {});
+const result = [ ];
+
+for(let i = 0; i < str.length; i++){
+  if(bracketsConfigObj[str[i]] === str[i] && !result.includes(str[i])){
+    result.push(str[i]);
+  } else if(Object.values(bracketsConfigObj).includes(str[i]) && bracketsConfigObj[str[i]] !== str[i]) {
+    result.push(str[i]);
+} else if(result[result.length - 1] === bracketsConfigObj[str[i]]){
+    result.pop();
+} else{
   return false;
 }
 
-
+}
+return result.length === 0;
 }
